253a9605d4322b5025e4a50702d60e135d8da969
Edwin <amdstorm@gmail.com>
2013-12-10 18:14:51 +0800

    updated readme for an easy guide to setup errbit

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,11 @@
 # http://michaelvanrooijen.com/articles/2011/06/01-more-concurrency-on-a-single-heroku-dyno-with-the-new-celadon-cedar-stack/
+APP_PATH = "/var/www/apps/errbit/current"
+working_directory APP_PATH
+
+stderr_path APP_PATH + "/log/unicorn.stderr.log"
+stdout_path APP_PATH + "/log/unicorn.stderr.log"
+
+pid APP_PATH + "/tmp/pids/unicorn.pid"
 
 worker_processes 3 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
